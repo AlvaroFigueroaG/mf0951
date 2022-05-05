758eec160759afeb96d1fdbc7e9cede409279faf
AlvaroFigueroaG <alvaro.figueroa@gmx.com>
2022-05-05 13:10:50 +0200

    ofuscados y minificados

---
+++ assets/js/index.ofuscado.2.js
@@ -0,0 +1,38 @@
+function _0x2282() {
+    var _0x1b8688 = ['getElementById', '951672GPAlZO', 'p1d', 'innerHTML', 'getHours', '284139SdkwbG', '75536WRcEBq', 'getMinutes', '4340897SkSHqy', '40792gQDrEu', '30ISRUSh', '279nDXwIw', '1534846aJVuyp', 'log', '225CDYuiN', 'compatMode', 'getSeconds', '38031QbosJK', '20uXIqwo'];
+    _0x2282 = function() {
+        return _0x1b8688;
+    };
+    return _0x2282();
+}
+
+function _0x1372(_0x156181, _0x3cdc47) {
+    var _0x228243 = _0x2282();
+    return _0x1372 = function(_0x137231, _0x54d07a) {
+        _0x137231 = _0x137231 - 0x6d;
+        var _0x1f41f4 = _0x228243[_0x137231];
+        return _0x1f41f4;
+    }, _0x1372(_0x156181, _0x3cdc47);
+}(function(_0x39397c, _0xf3a5fb) {
+    var _0x392535 = _0x1372,
+        _0x32637f = _0x39397c();
+    while (!![]) {
+        try {
+            var _0x35fc3c = parseInt(_0x392535(0x71)) / 0x1 + parseInt(_0x392535(0x78)) / 0x2 + parseInt(_0x392535(0x6d)) / 0x3 + parseInt(_0x392535(0x75)) / 0x4 * (-parseInt(_0x392535(0x7a)) / 0x5) + -parseInt(_0x392535(0x76)) / 0x6 * (parseInt(_0x392535(0x7d)) / 0x7) + parseInt(_0x392535(0x72)) / 0x8 * (parseInt(_0x392535(0x77)) / 0x9) + -parseInt(_0x392535(0x7e)) / 0xa * (parseInt(_0x392535(0x74)) / 0xb);
+            if (_0x35fc3c === _0xf3a5fb) break;
+            else _0x32637f['push'](_0x32637f['shift']());
+        } catch (_0x1aa54b) {
+            _0x32637f['push'](_0x32637f['shift']());
+        }
+    }
+}(_0x2282, 0x5e46f), window['onload'] = function() {
+    var _0xd93c98 = _0x1372;
+    document['getElementById']('p1c')[_0xd93c98(0x6f)] = document[_0xd93c98(0x7b)], document['getElementById']('p1c2')[_0xd93c98(0x6f)] = document['compatMode'], console[_0xd93c98(0x79)](dameHora()), document[_0xd93c98(0x7f)](_0xd93c98(0x6e))[_0xd93c98(0x6f)] = dameHora();
+});
+
+function dameHora() {
+    var _0x368c13 = _0x1372,
+        _0x66f640 = new Date(),
+        _0x50ef5d = _0x66f640[_0x368c13(0x70)]() + ':' + _0x66f640[_0x368c13(0x73)]() + ':' + _0x66f640[_0x368c13(0x7c)]();
+    return _0x50ef5d;
+}
